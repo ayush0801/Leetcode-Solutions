076ee1d166365e20278c903ec575147fe5e7e1a6
Ayush Jha <62278273+ayush0801@users.noreply.github.com>
2023-10-25 19:48:28 +0530

    Time: 2 ms (51.37%), Space: 6.3 MB (28.86%) - LeetHub

---
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int kthGrammar(int n, int k) {
+        bool areValuesSame = true; 
+
+        n = pow(2, n - 1);
+
+        while (n != 1) {
+            n /= 2;
+
+            if (k > n) {
+                k -= n;
+                areValuesSame = !areValuesSame;
+            }
+        }
+
+        return ((areValuesSame) ? 0 : 1) ;
+    }
+};
