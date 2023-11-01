be605c2930df08efe5e9b785e351e5c4decaef93
Ayush Jha <62278273+ayush0801@users.noreply.github.com>
2023-11-01 19:16:55 +0530

    Time: 27 ms (8.59%), Space: 26.3 MB (10.13%) - LeetHub

---
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_map<int, int> hash;
+    // int findmx(TreeNode* root){
+    //     if()
+    // }
+    void dfs(TreeNode* root){
+        if(!root)   return;
+        hash[root->val]++;
+        dfs(root->right);
+        dfs(root->left);
+    }
+    vector<int> findMode(TreeNode* root) {
+        vector<pair<int,int>> freq;
+        vector<int> ans;
+        dfs(root);
+        
+        for(auto x: hash){
+            auto p = make_pair(x.second, x.first);
+            freq.push_back(p);
+        }
+        
+        sort(freq.rbegin(), freq.rend());
+        int mx = freq[0].first;
+        
+        for(int i=0; i<freq.size(); i++){
+            if(freq[i].first==mx)   ans.push_back(freq[i].second);
+            else break;
+        }
+        
+        return ans;
+    }
+};
