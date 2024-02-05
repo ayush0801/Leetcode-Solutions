38159f720671e5def9988c280c55803ec94b9c46
Ayush Jha <62278273+ayush0801@users.noreply.github.com>
2024-02-05 20:39:29 +0530

    Time: 34 ms (51.74%), Space: 13.1 MB (17.78%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char, int> hash;
+        for(int i=0; i<s.size(); i++)
+            hash[s[i]]++;
+        
+        for(int i=0; i<s.size(); i++){
+            if(hash[s[i]]<2)
+                return i;
+        }
+        return -1;
+    }
+};
