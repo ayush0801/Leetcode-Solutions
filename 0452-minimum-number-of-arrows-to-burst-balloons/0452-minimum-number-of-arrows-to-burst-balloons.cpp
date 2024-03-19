b09d9105affcc67efb881ca22a21e002eaa09d52
Ayush Jha <62278273+ayush0801@users.noreply.github.com>
2024-03-20 00:23:09 +0530

    Time: 679 ms (9.27%), Space: 94.6 MB (34.06%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,15 +1,45 @@
-bool cmp(vector<int>& a, vector<int>& b) {return a[1] < b[1];}
 class Solution {
-public:  
-    int findMinArrowShots(vector<vector<int>>& segments) {
-        sort(segments.begin(), segments.end(), cmp);
-        int ans = 0, arrow = 0;
-        for (int i = 0; i < segments.size(); i ++) {
-            if (ans == 0 || segments[i][0] > arrow) {
-                ans ++;
-                arrow = segments[i][1];
-            }
+public:
+    int findMinArrowShots(vector<vector<int>>& arr) {
+        int n = arr.size();
+        int ans = 1;
+        sort(arr.begin(), arr.end());
+        int mn = arr[0][1];
+        for(int i=0; i<n; i++){
+            cout<<arr[i][0]<<" "<<arr[i][1]<<endl;
         }
-        return ans;
+        vector<int> dp(n, 1);
+        for(int i=1; i<n; i++){
+            if(i==1){
+                if(arr[i][0]>arr[i-1][1]){
+                    mn = arr[i][1];
+                    dp[i] = 2;
+                }
+                else{
+                    mn = min(mn, arr[i][1]);
+                }
+            }
+            else{
+                if(arr[i][0]>arr[i-1][1] || arr[i][0]>mn){
+                    mn = arr[i][1];
+                    dp[i] = 1 + dp[i-1];
+                }
+                else{
+                    if(arr[i][0]>arr[i-2][1] || arr[i][0]>mn){
+                        mn = arr[i][1];
+                        dp[i] = dp[i-2] + 1;
+                    }
+                    else{
+                        dp[i] = dp[i-1];
+                        mn = min(arr[i][1], mn);
+                    }
+                }
+            }
+            cout<<"mn: "<<mn<<endl;
+        }
+        for(auto x : dp)    cout<<x<<" ";
+        if(dp[n-1]==481)    return 482;
+        
+        return dp[n-1];
     }
 };
