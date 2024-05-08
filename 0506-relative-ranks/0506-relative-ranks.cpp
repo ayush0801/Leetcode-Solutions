f6dd1a340a1e139943e32080304e797c6657f13c
Ayush Jha <62278273+ayush0801@users.noreply.github.com>
2024-05-08 21:54:06 +0530

    Time: 15 ms (26.52%), Space: 18.4 MB (5.21%) - LeetHub

---
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& arr) {
+        int n = arr.size();
+        vector<vector<int>> temp;
+        vector<string> ans(n);
+        for(int i=0; i<n; i++){
+            temp.push_back({arr[i], i});
+            // cout<<temp[i][0]<<" "<<temp[i][1]<<endl;
+        }
+        sort(temp.rbegin(), temp.rend());
+        string s = "";
+        for(int i=0; i<n; i++){
+            // temp.push_back({arr[i], i});
+            cout<<temp[i][0]<<" "<<temp[i][1]<<endl;
+        }
+        for(int i=0; i<n; i++){
+            if(i==0){
+                s = "Gold Medal";
+            }
+            else if(i==1){
+                s = "Silver Medal";
+            }
+            else if(i==2){
+                s = "Bronze Medal";
+            }
+            else{
+                s = to_string(i+1);
+            }
+            ans[temp[i][1]] = s;
+        }
+        
+        return ans;
+    }
+};
