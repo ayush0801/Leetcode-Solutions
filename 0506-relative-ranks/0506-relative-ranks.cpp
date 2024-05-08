54ebde5ffcfa8a264637dd4063f8e31feb219fed
Ayush Jha <62278273+ayush0801@users.noreply.github.com>
2024-05-08 21:55:23 +0530

    Time: 7 ms (78.51%), Space: 18.5 MB (5.21%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.cpp
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -6,27 +6,15 @@ public:
         vector<string> ans(n);
         for(int i=0; i<n; i++){
             temp.push_back({arr[i], i});
-            // cout<<temp[i][0]<<" "<<temp[i][1]<<endl;
         }
         sort(temp.rbegin(), temp.rend());
         string s = "";
+
         for(int i=0; i<n; i++){
-            // temp.push_back({arr[i], i});
-            cout<<temp[i][0]<<" "<<temp[i][1]<<endl;
-        }
-        for(int i=0; i<n; i++){
-            if(i==0){
-                s = "Gold Medal";
-            }
-            else if(i==1){
-                s = "Silver Medal";
-            }
-            else if(i==2){
-                s = "Bronze Medal";
-            }
-            else{
-                s = to_string(i+1);
-            }
+            if(i==0)        s = "Gold Medal";
+            else if(i==1)   s = "Silver Medal";
+            else if(i==2)   s = "Bronze Medal";
+            else            s = to_string(i+1);
             ans[temp[i][1]] = s;
         }
         
