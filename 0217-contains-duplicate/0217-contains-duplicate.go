a96398c003ba3da9dd277d9ee00e998799229750
Ayush Jha <62278273+ayush0801@users.noreply.github.com>
2024-05-26 17:10:49 +0530

    Time: 87 ms (29.87%), Space: 16.9 MB (5.03%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.go
+++ 0217-contains-duplicate/0217-contains-duplicate.go
@@ -1,13 +1,13 @@
 func containsDuplicate(arr []int) bool {
     n := len(arr)
     
-    hash := make(map[int]int)
+    hash := make(map[int]bool)
     
     for i := 0; i<n; i++ {
-        if hash[arr[i]]>0 {
+        if hash[arr[i]]==true {
             return true
         }
-        hash[arr[i]]++;
+        hash[arr[i]] = true;
     }
     return false
 }
