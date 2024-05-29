9cef6c1128786f81282cc64d9f4fb4685afd1686
Ayush Jha <62278273+ayush0801@users.noreply.github.com>
2024-05-30 00:19:53 +0530

    Time: 3 ms (90.70%), Space: 4.3 MB (31.32%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.go
@@ -0,0 +1,13 @@
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+
+	for i, num := range nums {
+		
+		if idx, ok := m[target-num]; ok {
+			
+			return []int{idx, i}
+		}
+		m[num] = i
+	}
+	return []int{0, 0}
+}
