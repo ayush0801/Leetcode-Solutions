b0630a38cd05f62a05e0de909b364156e299d9bd
Ayush Jha <62278273+ayush0801@users.noreply.github.com>
2024-08-29 08:15:37 +0530

    Time: 2 ms (92.69%), Space: 8.8 MB (83.99%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,60 +1,19 @@
 class Solution {
 public:
-    // void toLower(string s){
-    //     for(auto x : s){
-    //         x = to_lower(x);
-    //     }
-    // }
-    bool isAlphaNumeric(char c){
-        if(int(c)>64 and int(c)<91 || (int(c)>96 and int(c)<123))
-            return true;
-        return false;
-    }
-    bool isPalindrome(string str) {
-        string s="";
-        for(auto x : str){
-            if(!isalnum(x)){
-                continue;
-            }
-            else{
-                x = tolower(x);
-                s += x;
-            }
-            
-            // cout<<x;
-        }
-        int n = s.size();
-        int i = 0;
-        int j = n-1;
-
-        // cout<<endl;
+    bool isPalindrome(string s) {
+        int i=0;
+        int j = s.size()-1;
         while(i<=j){
-            // cout<<s[i]<<" "<<s[j]<<endl;
-            if(s[i]!=s[j]){
-                return false;
+            if(isalnum(s[i]) and isalnum(s[j])){
+                if(tolower(s[i])==tolower(s[j])){
+                    i++;
+                    j--;
+                }
+                else    return false;
             }
-            i++;
-            j--;
+            else if(!isalnum(s[i])) i++;
+            else if(!isalnum(s[j])) j--;
         }
         return true;
-        
-        
-        // while(i<=j){
-        //     if(!isalnum(s[i])){
-        //         cout<<"s[i]: "<<s[i]<<endl;
-        //         i++;
-        //     }
-        //     else if(!isalnum(s[j])){
-        //         cout<<"s[j]: "<<s[j]<<endl;
-        //         j--;
-        //     }
-        //     if(s[i]!=s[j]){
-        //         return false;
-        //     }
-        //     i++;
-        //     j--;
-        //     cout<<s[i]<<" "<<s[j]<<endl;
-        // }
-        // return true;
     }
 };
